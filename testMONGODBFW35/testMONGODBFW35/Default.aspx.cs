91d1be44a5e7593da7f6a069a2eac941f858d064
cagako85 <cagatayyapici@windowslive.com>
2013-08-14 15:02:19 +0300

    newRelease

--- testMONGODBFW35/testMONGODBFW35/Default.aspx.cs
+++ testMONGODBFW35/testMONGODBFW35/Default.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
 
 namespace testMONGODBFW35
 {
@@ -12,6 +16,35 @@ namespace testMONGODBFW35
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            var connectionString = "mongodb://localhost";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("test");
+            var collection = database.GetCollection<Entity>("entities");
+
+            var entity = new Entity { Name = "Tom" };
+            collection.Insert(entity);
+            var id = entity.Id;
+          
+            var query = Query<Entity>.EQ(r => r.Id, id);
+            entity = collection.FindOne(query);
+
+            Response.Write(entity.Name);
+            //entity.Name = "Dick";
+            //collection.Save(entity);
+
+            //var update = Update<Entity>.Set(w => w.Name, "Harry");
+            //collection.Update(query, update);
+
+            //collection.Remove(query);
+
         }
+
+        public class Entity
+        {
+            public ObjectId Id { get; set; }
+            public string Name { get; set; }
+        }
+
     }
 }
