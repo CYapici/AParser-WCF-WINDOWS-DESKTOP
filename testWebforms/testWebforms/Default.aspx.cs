5bf05719da330f7e3d6416126b54319fa2079603
cagako85 <cagatayyapici@windowslive.com>
2013-08-26 11:04:06 +0300

    nR

---
+++ testWebforms/testWebforms/Default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Twilio;
+
+namespace testWebforms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string ACCOUNT_SID = "AC79b803fc2b40d78256710d5f35db3e4c";
+            string AUTH_TOKEN = "6c406cef49041d34ab9b558f3f549f64";
+
+            TwilioRestClient client = new TwilioRestClient(ACCOUNT_SID, AUTH_TOKEN);
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    client.SendSmsMessage("+14242752396", "+905327662418", " cagatay from c#");
+                }
+                catch (Exception ex)
+                {
+
+                    throw ex;
+                }
+            }
+
+
+        }
+    }
+}
---
+++ testWindowsForms/testWindowsForms/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace testWindowsForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            //Uri ur = new Uri("https://secure.sahibinden.com/giris/?return_url=%2Findex.php");
+            //this.webBrowser1.Url = ur;
+            //dynamic doc = this.webBrowser1.Document;
+
+            //MessageBox.Show(doc.body.outerHtml);
+
+            webBrowser1.Url = new Uri("https://secure.sahibinden.com/giris/?return_url=%2Findex.php");
+            //webBrowser1.Document.GetElementById("username").SetAttribute("value", "cagatayyapici@windowslive.com");
+            //webBrowser1.Document.GetElementById("password").SetAttribute("value", "kemence85");
+
+        }
+    }
+}
